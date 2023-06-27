216d85e1f0377f8b24001c5c09647fa56b5d5058
Yash Gangwar <gangwaryash06@gmail.com>
2023-06-27 10:10:04 +0000

    create activity working

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -188,7 +188,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'tester7558'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,18 +1,18 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, email, cognito_user_id)
-VALUES
-  ('Andrew Brown', 'andrewbrown' , 'andrew@gmail.com','MOCK'),
-  ('Andrew Bayko', 'bayko', 'bayko@gmail.com','MOCK');
+-- INSERT INTO public.users (display_name, handle, email, cognito_user_id)
+-- VALUES
+--   ('Andrew Brown', 'andrewbrown' , 'andrew@gmail.com','MOCK'),
+--   ('Andrew Bayko', 'bayko', 'bayko@gmail.com','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
-    'This was andrewbrown imported as seed data!',
-    current_timestamp + interval '10 day'
-  ),
-  (
-    (SELECT uuid from public.users WHERE users.handle = 'bayko' LIMIT 1),
-    'This was bayko imported as seed data!',
+    (SELECT uuid from public.users WHERE users.handle = 'tester7558' LIMIT 1),
+    'This was tester7558 imported as seed data!',
     current_timestamp + interval '10 day'
   )
+  -- (
+  --   (SELECT uuid from public.users WHERE users.handle = 'bayko' LIMIT 1),
+  --   'This was bayko imported as seed data!',
+  --   current_timestamp + interval '10 day'
+  -- )
